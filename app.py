a4d9ecfc0771d8ad1635c2901a5affef141d4a17
Nithin <47273351+jns1406@users.noreply.github.com>
2023-03-30 07:48:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ add_bg_from_url()
 
 
 
-data_url = ("https://github.com/jns1406/NYC_motor_collison_StreamlitWebApp/blob/main/NYC_Motor_Collision_2022.csv")
+data_url = ("https://github.com/jns1406/NYC_motor_collison_StreamlitWebApp/blob/main/NYC_Motor_Collision_2022.csv?raw=True")
 
 
 
